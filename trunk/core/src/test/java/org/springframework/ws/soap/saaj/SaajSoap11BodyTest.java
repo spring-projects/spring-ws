15a83411f10c104005b69541a6ed4a7a8fa0a107
Arjen Poutsma <apoutsma@vmware.com>
2006-12-31 20:42:41 +0000

    Refactored SAAJ implementation from two separate class hierarchies (SAAJ 1.2 and SAAJ 1.3) into one single hierarchy, using bridge pattern (SaajImplementation).

--- trunk/core/src/test/java/org/springframework/ws/soap/saaj/SaajSoap11BodyTest.java
+++ trunk/core/src/test/java/org/springframework/ws/soap/saaj/SaajSoap11BodyTest.java
@@ -14,20 +14,21 @@
  * limitations under the License.
  */
 
-package org.springframework.ws.soap.saaj.saaj12;
+package org.springframework.ws.soap.saaj;
 
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPConstants;
 import javax.xml.soap.SOAPMessage;
 
-import org.springframework.ws.soap.SoapHeader;
-import org.springframework.ws.soap.soap11.AbstractSoap11HeaderTestCase;
+import org.springframework.ws.soap.SoapBody;
+import org.springframework.ws.soap.soap11.AbstractSoap11BodyTestCase;
 
-public class Saaj12Soap11HeaderTest extends AbstractSoap11HeaderTestCase {
+public class SaajSoap11BodyTest extends AbstractSoap11BodyTestCase {
 
-    protected SoapHeader createSoapHeader() throws Exception {
+    protected SoapBody createSoapBody() throws Exception {
         MessageFactory messageFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_1_PROTOCOL);
         SOAPMessage saajMessage = messageFactory.createMessage();
-        return new Saaj12SoapHeader(saajMessage.getSOAPHeader());
+        return new SaajSoap11Body(saajMessage.getSOAPBody());
     }
+
 }
--- trunk/core/src/test/java/org/springframework/ws/soap/saaj/SaajSoap12MessageTest.java
+++ trunk/core/src/test/java/org/springframework/ws/soap/saaj/SaajSoap12MessageTest.java
@@ -1,4 +1,20 @@
-package org.springframework.ws.soap.saaj.saaj13;
+/*
+ * Copyright 2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ws.soap.saaj;
 
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPConstants;
@@ -11,14 +27,14 @@ import org.springframework.ws.soap.soap12.AbstractSoap12MessageTestCase;
 import org.springframework.xml.transform.StringResult;
 import org.springframework.xml.transform.StringSource;
 
-public class Saaj13Soap12MessageTest extends AbstractSoap12MessageTestCase {
+public class SaajSoap12MessageTest extends AbstractSoap12MessageTestCase {
 
     private SOAPMessage saajMessage;
 
     protected SoapMessage createSoapMessage() throws Exception {
         MessageFactory messageFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
         saajMessage = messageFactory.createMessage();
-        return new Saaj13SoapMessage(saajMessage);
+        return new SaajSoapMessage(saajMessage);
     }
 
     public void testGetPayloadSource() throws Exception {
