983bfe6bc6e70db89ffd466765cb161b9d1fffee
Rick Evans <springboy>
2007-05-28 13:21:05 +0000

    Added missing Apache licence header.
    Polishing of Javadoc.

--- core/src/main/java/org/springframework/ws/transport/WebServiceMessageSender.java
+++ core/src/main/java/org/springframework/ws/transport/WebServiceMessageSender.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ws.transport;
 
 import java.io.IOException;
@@ -5,10 +21,11 @@ import java.io.IOException;
 import org.springframework.ws.WebServiceMessage;
 
 /**
- * Defines the methods for classes capable of sending and receiving {@link WebServiceMessage} instances across a
- * transport.
- * <p/>
- * The <code>WebServiceMessageSender</code> is basically a factory for {@link WebServiceConnection} objects.
+ * Defines the methods for classes capable of sending and receiving
+ * {@link WebServiceMessage} instances across a transport.
+ *
+ * <p>The <code>WebServiceMessageSender</code> is basically a factory for
+ * {@link WebServiceConnection} objects.
  *
  * @author Arjen Poutsma
  * @see WebServiceConnection
@@ -25,10 +42,10 @@ public interface WebServiceMessageSender {
     WebServiceConnection createConnection(String uri) throws IOException;
 
     /**
-     * Indicates whether the message sender supports the given URI.
+     * Does this {@link {@link WebServiceMessageSender}} support the supplied URI?
      *
-     * @param uri the URI
-     * @return true
+     * @param uri the URI to be checked
+     * @return <code>true</code> if this <code>WebServiceMessageSender</code> supports the supplied URI
      */
     boolean supports(String uri);
 
