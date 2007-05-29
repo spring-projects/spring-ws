536fc0a6188a938601c9218f4845ae0df0d41dfb
Rick Evans <springboy>
2007-05-29 21:24:58 +0000

    Added missing class-level Javadoc while addressing [SWS-132].

--- trunk/core/src/main/java/org/springframework/ws/wsdl/wsdl11/Wsdl11Definition.java
+++ trunk/core/src/main/java/org/springframework/ws/wsdl/wsdl11/Wsdl11Definition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 the original author or authors.
+ * Copyright 2006-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,12 @@ package org.springframework.ws.wsdl.wsdl11;
 import org.springframework.ws.wsdl.WsdlDefinition;
 
 /**
+ * Models a WSDL 1.1-specific {@link WsdlDefinition}.
+ *
+ * <p>Just a marker interface at the moment.
+ *
  * @author Arjen Poutsma
+ * @see Wsdl11DefinitionBuilder
  */
 public interface Wsdl11Definition extends WsdlDefinition {
 
