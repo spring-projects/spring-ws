884073ce0b5fefdb50e09bdf45c30d619f3d13e1
Arjen Poutsma <apoutsma@vmware.com>
2007-09-26 08:34:51 +0000

    Copyright

--- trunk/security/src/main/java/org/springframework/ws/soap/security/WsSecurityFaultException.java
+++ trunk/security/src/main/java/org/springframework/ws/soap/security/WsSecurityFaultException.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ws.soap.security;
 
 import javax.xml.namespace.QName;
@@ -16,9 +32,7 @@ public abstract class WsSecurityFaultException extends WsSecurityException {
 
     private String faultActor;
 
-    /**
-     * Construct a new <code>WsSecurityFaultException</code> with the given fault code, string, and actor.
-     */
+    /** Construct a new <code>WsSecurityFaultException</code> with the given fault code, string, and actor. */
     public WsSecurityFaultException(QName faultCode, String faultString, String faultActor) {
         super(faultString);
         this.faultCode = faultCode;
@@ -26,23 +40,17 @@ public abstract class WsSecurityFaultException extends WsSecurityException {
         this.faultActor = faultActor;
     }
 
-    /**
-     * Returns the fault code for the exception.
-     */
+    /** Returns the fault code for the exception. */
     public QName getFaultCode() {
         return faultCode;
     }
 
-    /**
-     * Returns the fault string for the exception.
-     */
+    /** Returns the fault string for the exception. */
     public String getFaultString() {
         return faultString;
     }
 
-    /**
-     * Returns the fault actor for the exception.
-     */
+    /** Returns the fault actor for the exception. */
     public String getFaultActor() {
         return faultActor;
     }
