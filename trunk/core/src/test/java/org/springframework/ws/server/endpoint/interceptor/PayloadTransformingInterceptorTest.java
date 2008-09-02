f601ebefb69bfca8c68e4ab9734398f88225be6f
Arjen Poutsma <apoutsma@vmware.com>
2008-09-02 15:36:08 +0000

    SWS-412

--- trunk/core/src/test/java/org/springframework/ws/server/endpoint/interceptor/PayloadTransformingInterceptorTest.java
+++ trunk/core/src/test/java/org/springframework/ws/server/endpoint/interceptor/PayloadTransformingInterceptorTest.java
@@ -16,19 +16,27 @@
 
 package org.springframework.ws.server.endpoint.interceptor;
 
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXSource;
 
 import org.custommonkey.xmlunit.XMLTestCase;
 import org.custommonkey.xmlunit.XMLUnit;
+
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.ws.MockWebServiceMessage;
 import org.springframework.ws.MockWebServiceMessageFactory;
 import org.springframework.ws.context.DefaultMessageContext;
 import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.pox.dom.DomPoxMessage;
+import org.springframework.ws.pox.dom.DomPoxMessageFactory;
+import org.springframework.ws.soap.saaj.SaajSoapMessage;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 import org.springframework.xml.sax.SaxUtils;
+import org.springframework.xml.transform.ResourceSource;
 import org.springframework.xml.transform.StringResult;
 
 public class PayloadTransformingInterceptorTest extends XMLTestCase {
@@ -61,9 +69,9 @@ public class PayloadTransformingInterceptorTest extends XMLTestCase {
 
         boolean result = interceptor.handleRequest(context, null);
         assertTrue("Invalid interceptor result", result);
-        StringResult stringResult = new StringResult();
-        transformer.transform(new SAXSource(SaxUtils.createInputSource(output)), stringResult);
-        assertXMLEqual(stringResult.toString(), request.getPayloadAsString());
+        StringResult expected = new StringResult();
+        transformer.transform(new SAXSource(SaxUtils.createInputSource(output)), expected);
+        assertXMLEqual(expected.toString(), request.getPayloadAsString());
     }
 
     public void testHandleRequestNoXslt() throws Exception {
@@ -74,9 +82,9 @@ public class PayloadTransformingInterceptorTest extends XMLTestCase {
 
         boolean result = interceptor.handleRequest(context, null);
         assertTrue("Invalid interceptor result", result);
-        StringResult stringResult = new StringResult();
-        transformer.transform(new SAXSource(SaxUtils.createInputSource(input)), stringResult);
-        assertXMLEqual(stringResult.toString(), request.getPayloadAsString());
+        StringResult expected = new StringResult();
+        transformer.transform(new SAXSource(SaxUtils.createInputSource(input)), expected);
+        assertXMLEqual(expected.toString(), request.getPayloadAsString());
     }
 
     public void testHandleResponse() throws Exception {
@@ -89,9 +97,9 @@ public class PayloadTransformingInterceptorTest extends XMLTestCase {
 
         boolean result = interceptor.handleResponse(context, null);
         assertTrue("Invalid interceptor result", result);
-        StringResult stringResult = new StringResult();
-        transformer.transform(new SAXSource(SaxUtils.createInputSource(output)), stringResult);
-        assertXMLEqual(stringResult.toString(), response.getPayloadAsString());
+        StringResult expected = new StringResult();
+        transformer.transform(new SAXSource(SaxUtils.createInputSource(output)), expected);
+        assertXMLEqual(expected.toString(), response.getPayloadAsString());
     }
 
     public void testHandleResponseNoXslt() throws Exception {
@@ -104,9 +112,44 @@ public class PayloadTransformingInterceptorTest extends XMLTestCase {
 
         boolean result = interceptor.handleResponse(context, null);
         assertTrue("Invalid interceptor result", result);
-        StringResult stringResult = new StringResult();
-        transformer.transform(new SAXSource(SaxUtils.createInputSource(input)), stringResult);
-        assertXMLEqual(stringResult.toString(), response.getPayloadAsString());
+        StringResult expected = new StringResult();
+        transformer.transform(new SAXSource(SaxUtils.createInputSource(input)), expected);
+        assertXMLEqual(expected.toString(), response.getPayloadAsString());
+    }
+
+    public void testSaaj() throws Exception {
+        interceptor.setRequestXslt(xslt);
+        interceptor.afterPropertiesSet();
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage saajMessage = messageFactory.createMessage();
+        SaajSoapMessage message = new SaajSoapMessage(saajMessage);
+        transformer.transform(new ResourceSource(input), message.getPayloadResult());
+        MessageContext context = new DefaultMessageContext(message, new SaajSoapMessageFactory(messageFactory));
+
+        assertTrue("Invalid interceptor result", interceptor.handleRequest(context, null));
+        StringResult expected = new StringResult();
+        transformer.transform(new SAXSource(SaxUtils.createInputSource(output)), expected);
+        StringResult result = new StringResult();
+        transformer.transform(message.getPayloadSource(), result);
+        assertXMLEqual(expected.toString(), result.toString());
+
+    }
+
+    public void testPox() throws Exception {
+        interceptor.setRequestXslt(xslt);
+        interceptor.afterPropertiesSet();
+        DomPoxMessageFactory factory = new DomPoxMessageFactory();
+        DomPoxMessage message = (DomPoxMessage) factory.createWebServiceMessage();
+        transformer.transform(new ResourceSource(input), message.getPayloadResult());
+        MessageContext context = new DefaultMessageContext(message, factory);
+
+        assertTrue("Invalid interceptor result", interceptor.handleRequest(context, null));
+        StringResult expected = new StringResult();
+        transformer.transform(new SAXSource(SaxUtils.createInputSource(output)), expected);
+        StringResult result = new StringResult();
+        transformer.transform(message.getPayloadSource(), result);
+        assertXMLEqual(expected.toString(), result.toString());
+
     }
 
     public void testNoStylesheetsSet() throws Exception {
