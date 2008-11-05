dcb812fe6c69b66fc9bb2fe073b0c100dc0c01c1
Arjen Poutsma <apoutsma@vmware.com>
2008-11-05 10:18:50 +0000

    SWS-444

--- security/src/test/java/org/springframework/ws/soap/security/xwss/XwssMessageInterceptorEncryptTest.java
+++ security/src/test/java/org/springframework/ws/soap/security/xwss/XwssMessageInterceptorEncryptTest.java
@@ -20,6 +20,7 @@ import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.xml.soap.SOAPMessage;
 
+import com.sun.xml.wss.impl.callback.DecryptionKeyCallback;
 import com.sun.xml.wss.impl.callback.EncryptionKeyCallback;
 
 import org.springframework.core.io.ClassPathResource;
@@ -96,11 +97,6 @@ public class XwssMessageInterceptorEncryptTest extends AbstractXwssMessageInterc
                 "/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security/xenc:EncryptedKey", result);
     }
 
-    /*
-    This test fails under JDK 1.6, giving a IllegalArgumentException:
-    com.sun.xml.internal.messaging.saaj.soap.LocalStrings != com.sun.xml.messaging.saaj.soap.LocalStrings
-
-     No idea how to fix it
     public void testDecrypt() throws Exception {
         interceptor.setPolicyConfiguration(new ClassPathResource("decrypt-config.xml", getClass()));
         CallbackHandler handler = new AbstractCallbackHandler() {
@@ -126,10 +122,10 @@ public class XwssMessageInterceptorEncryptTest extends AbstractXwssMessageInterc
         interceptor.setCallbackHandler(handler);
         interceptor.afterPropertiesSet();
         SaajSoapMessage message = loadSaajMessage("encrypted-soap.xml");
-        interceptor.validateMessage(message);
+        interceptor.validateMessage(message, null);
         SOAPMessage result = message.getSaajMessage();
         assertNotNull("No result returned", result);
         assertXpathNotExists("Security Header not removed", "/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security", result);
-    }*/
+    }
 
 }
