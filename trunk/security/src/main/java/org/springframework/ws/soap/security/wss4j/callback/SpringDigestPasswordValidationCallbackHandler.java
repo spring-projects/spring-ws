6ad6126607d491fd9009b357545bd5fd75d3fc1f
Arjen Poutsma <apoutsma@vmware.com>
2009-05-19 10:19:34 +0000

    Changed from using WSS4J getIdentifer() to getIdentifier().

--- trunk/security/src/main/java/org/springframework/ws/soap/security/wss4j/callback/SpringDigestPasswordValidationCallbackHandler.java
+++ trunk/security/src/main/java/org/springframework/ws/soap/security/wss4j/callback/SpringDigestPasswordValidationCallbackHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 the original author or authors.
+ * Copyright 2002-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public class SpringDigestPasswordValidationCallbackHandler extends AbstractWsPas
     }
 
     protected void handleUsernameToken(WSPasswordCallback callback) throws IOException, UnsupportedCallbackException {
-        String identifier = callback.getIdentifer();
+        String identifier = callback.getIdentifier();
         UserDetails user = loadUserDetails(identifier);
         if (user != null) {
             callback.setPassword(user.getPassword());
