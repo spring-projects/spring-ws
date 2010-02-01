043ced5ce597b153e45ab07a1d554fef25a46819
Arjen Poutsma <apoutsma@vmware.com>
2010-02-01 11:25:25 +0000

    - Removed packages scheduled for removal in 2.0. - Added @Deprecated annotations.

--- trunk/core/src/main/java/org/springframework/ws/server/endpoint/AbstractMarshallingPayloadEndpoint.java
+++ trunk/core/src/main/java/org/springframework/ws/server/endpoint/AbstractMarshallingPayloadEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 the original author or authors.
+ * Copyright 2005-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -191,6 +191,7 @@ public abstract class AbstractMarshallingPayloadEndpoint implements MessageEndpo
      * @deprecated as of Spring Web Services 1.5: {@link #afterPropertiesSet()} is no longer final, so this can safely
      *             be overriden in subclasses
      */
+    @Deprecated
     public void afterMarshallerSet() throws Exception {
     }
 
