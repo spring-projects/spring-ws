5d354f4f5b5e91b949256eae2695925765053117
Arjen Poutsma <apoutsma@vmware.com>
2010-05-03 10:24:24 +0000

    SWS-351 - Arbitrary parameter injection for @Endpoints

--- core/src/test/java/org/springframework/ws/server/endpoint/adapter/method/AbstractPayloadMethodProcessorTestCase.java
+++ core/src/test/java/org/springframework/ws/server/endpoint/adapter/method/AbstractPayloadMethodProcessorTestCase.java
@@ -29,7 +29,7 @@ import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public abstract class AbstractPayloadMethodProcessorTest extends AbstractMethodArgumentResolverTest {
+public abstract class AbstractPayloadMethodProcessorTestCase extends AbstractMethodArgumentResolverTest {
 
     private AbstractPayloadSourceMethodProcessor processor;
 
