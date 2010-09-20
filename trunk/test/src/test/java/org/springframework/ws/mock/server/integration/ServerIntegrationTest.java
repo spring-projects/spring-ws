003466789d3e07e2232e684c878848bca6eb18e0
Arjen Poutsma <apoutsma@vmware.com>
2010-09-20 12:30:39 +0000

    SWS-632 - Create Server-Side testing framework

--- trunk/test/src/test/java/org/springframework/ws/mock/server/integration/ServerIntegrationTest.java
+++ trunk/test/src/test/java/org/springframework/ws/mock/server/integration/ServerIntegrationTest.java
@@ -14,57 +14,35 @@
  * limitations under the License.
  */
 
-package org.springframework.ws.mock.client.integration;
+package org.springframework.ws.mock.server.integration;
 
 import javax.xml.transform.Source;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.xml.transform.StringSource;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.ws.mock.client.WebServiceMock.*;
+import static org.springframework.ws.mock.server.WebServiceMock.*;
 
 /**
- * Integration test for client-side WebService testing. In different package so we can't use the package-protected
- * classes.
- *
  * @author Arjen Poutsma
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("integration-test.xml")
-public class IntegrationTest {
-
-    @Autowired
-    private WebServiceTemplate webServiceTemplate;
-
-    @Before
-    public void setUpMocks() throws Exception {
-        mockWebServiceTemplate(webServiceTemplate);
-    }
+public class ServerIntegrationTest {
 
     @Test
     public void basic() throws Exception {
-        Source expectedRequestPayload = new StringSource("<customerCountRequest xmlns='http://springframework.org/client'>" +
+        Source requestPayload = new StringSource("<customerCountRequest xmlns='http://springframework.org/client'>" +
                 "<customerName>John Doe</customerName>" + "</customerCountRequest>");
         Source responsePayload = new StringSource("<customerCountResponse xmlns='http://springframework.org/client'>" +
                 "<customerCount>10</customerCount>" + "</customerCountResponse>");
 
-        expect(payload(expectedRequestPayload)).andRespond(withPayload(responsePayload));
-
-        CustomerCountRequest request = new CustomerCountRequest();
-        request.setCustomerName("John Doe");
-
-        CustomerCountResponse response = (CustomerCountResponse) webServiceTemplate.marshalSendAndReceive(request);
-        assertEquals(10, response.getCustomerCount());
-        
-        verifyConnections();
+        receiveMessage(withPayload(requestPayload)).andExpect(payload(responsePayload));
     }
+
 
 }
