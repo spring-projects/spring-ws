1c61a168148a31c04f35c2168c0d15a8b2cd8545
Arjen Poutsma <apoutsma@vmware.com>
2010-10-17 14:02:51 +0000

    Server-side testing

--- core/src/main/java/org/springframework/ws/soap/saaj/SaajSoapMessageFactory.java
+++ core/src/main/java/org/springframework/ws/soap/saaj/SaajSoapMessageFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2010 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -124,7 +124,7 @@ public class SaajSoapMessageFactory implements SoapMessageFactory<SaajSoapMessag
         }
     }
 
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet() {
         if (messageFactory == null) {
             try {
                 if (SaajUtils.getSaajVersion() >= SaajUtils.SAAJ_13) {
