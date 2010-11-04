4fc43324a912296e0842282c169e3d08952c343f
Arjen Poutsma <apoutsma@vmware.com>
2010-11-04 11:43:31 +0000

    Polishing

--- trunk/test/src/test/java/org/springframework/ws/test/client/ErrorResponseCreatorTest.java
+++ trunk/test/src/test/java/org/springframework/ws/test/client/ErrorResponseCreatorTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ErrorResponseCallbackTest {
+public class ErrorResponseCreatorTest {
 
     @Test
     public void callback() throws IOException {
