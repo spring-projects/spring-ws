80bdf186d8ac2556fa21d0f2169c898f129027c2
Arjen Poutsma <apoutsma@vmware.com>
2010-12-13 09:56:55 +0000

    SWS-653 - Make sure samples are included in release

--- trunk/release.sh
+++ trunk/release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn -Dmaven.test.skip=true clean javadoc:javadoc docbkx:generate-html docbkx:generate-pdf install
+mvn -Dmaven.test.skip=true -P release clean javadoc:javadoc docbkx:generate-html docbkx:generate-pdf install
