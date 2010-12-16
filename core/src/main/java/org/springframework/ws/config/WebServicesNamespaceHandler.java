df9d82ac0f8893c94fde9cdfb88065ebbfe81bd6
Arjen Poutsma <apoutsma@vmware.com>
2010-12-16 16:46:28 +0000

    SWS-667 - Improve SWS namespace

--- core/src/main/java/org/springframework/ws/config/WebServicesNamespaceHandler.java
+++ core/src/main/java/org/springframework/ws/config/WebServicesNamespaceHandler.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2007 the original author or authors.
+ * Copyright 2005-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,7 +28,10 @@ import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 public class WebServicesNamespaceHandler extends NamespaceHandlerSupport {
 
     public void init() {
+        registerBeanDefinitionParser("annotation-driven", new AnnotationDrivenBeanDefinitionParser());
         registerBeanDefinitionParser("marshalling-endpoints", new MarshallingEndpointsBeanDefinitionParser());
         registerBeanDefinitionParser("xpath-endpoints", new XPathEndpointsBeanDefinitionParser());
+        registerBeanDefinitionParser("static-wsdl", new StaticWsdlBeanDefinitionParser());
+        registerBeanDefinitionParser("dynamic-wsdl", new DynamicWsdlBeanDefinitionParser());
     }
 }
