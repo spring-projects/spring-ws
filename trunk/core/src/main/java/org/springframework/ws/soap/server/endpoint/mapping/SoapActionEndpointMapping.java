497cf29b3e2330c3faba5e03ff6486d63deafb25
Arjen Poutsma <apoutsma@vmware.com>
2010-12-16 16:45:02 +0000

    Minor fixes

--- trunk/core/src/main/java/org/springframework/ws/soap/server/endpoint/mapping/SoapActionEndpointMapping.java
+++ trunk/core/src/main/java/org/springframework/ws/soap/server/endpoint/mapping/SoapActionEndpointMapping.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -49,7 +49,9 @@ import org.springframework.ws.soap.server.SoapEndpointMapping;
  *
  * @author Arjen Poutsma
  * @since 1.0.0
+ * @deprecated as of Spring Web Services 2.0, in favor of {@link SoapActionAnnotationMethodEndpointMapping}.
  */
+@Deprecated
 public class SoapActionEndpointMapping extends AbstractMapBasedEndpointMapping implements SoapEndpointMapping {
 
     private String[] actorsOrRoles;
