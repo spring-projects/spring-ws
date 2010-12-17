b27148951dba0dd83e2fc446b8f61f5d7b2950b5
Arjen Poutsma <apoutsma@vmware.com>
2010-12-17 13:47:37 +0000

    Fixing build.

--- core/src/test/java/org/springframework/ws/server/endpoint/mapping/MapBasedSoapEndpointMappingTest.java
+++ core/src/test/java/org/springframework/ws/server/endpoint/mapping/MapBasedSoapEndpointMappingTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -83,7 +83,7 @@ public class MapBasedSoapEndpointMappingTest {
         mapping.setValidKeys(new String[]{"endpoint1", "endpoint2"});
 
         mapping.setEndpointMap(endpointMap);
-        mapping.initApplicationContext();
+        mapping.setApplicationContext(new StaticApplicationContext());
 
         // try endpoint1
         mapping.setKey("endpoint1");
