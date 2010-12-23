3a4acf1aeb1705932a5bf4b4c43f81f75864d593
Arjen Poutsma <apoutsma@vmware.com>
2010-12-23 13:16:45 +0000

    Released Spring Web Services 2.0.0-RC2

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+MAVEN_OPTS="-Djava.awt.headless=true -Xmx512m -Xms64m -XX:MaxPermSize=128m"
 mvn -Dmaven.test.skip=true -P release clean javadoc:javadoc docbkx:generate-html docbkx:generate-pdf install
