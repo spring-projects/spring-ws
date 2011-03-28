7b1862ad1aa31a67ad222ef9911fe935019cffe2
Arjen Poutsma <apoutsma@vmware.com>
2011-03-28 09:31:19 +0000

    SWS-699 - XomPayloadMethodProcessor is not added methodReturnValueHandlers in DefaultMethodEndpointAdapter

--- core/src/main/java/org/springframework/ws/server/endpoint/adapter/DefaultMethodEndpointAdapter.java
+++ core/src/main/java/org/springframework/ws/server/endpoint/adapter/DefaultMethodEndpointAdapter.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2005-2010 the original author or authors.
+ * Copyright 2005-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -164,7 +164,7 @@ public class DefaultMethodEndpointAdapter extends AbstractMethodEndpointAdapter
                 methodReturnValueHandlers.add(new JDomPayloadMethodProcessor());
             }
             if (isPresent(XOM_CLASS_NAME)) {
-                methodArgumentResolvers.add(new XomPayloadMethodProcessor());
+                methodReturnValueHandlers.add(new XomPayloadMethodProcessor());
             }
             if (logger.isDebugEnabled()) {
                 logger.debug("No MethodReturnValueHandlers set, using defaults: " + methodReturnValueHandlers);
