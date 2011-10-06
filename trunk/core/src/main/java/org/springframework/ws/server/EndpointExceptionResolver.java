c7b86cf0280645a6c296165b856368ddf4b0d56d
Arjen Poutsma <apoutsma@vmware.com>
2011-10-06 10:10:42 +0000

    SWS-721 - EndpointExceptionResolver mentions SOAP but this interface is not necessarily SOAP releated

--- trunk/core/src/main/java/org/springframework/ws/server/EndpointExceptionResolver.java
+++ trunk/core/src/main/java/org/springframework/ws/server/EndpointExceptionResolver.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2005 the original author or authors.
+ * Copyright 2005-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,8 +19,7 @@ package org.springframework.ws.server;
 import org.springframework.ws.context.MessageContext;
 
 /**
- * Defines the interface for objects than can resolve exceptions thrown during endpoint execution, resulting in SOAP
- * messages.
+ * Defines the interface for objects than can resolve exceptions thrown during endpoint execution.
  *
  * @author Arjen Poutsma
  * @since 1.0.0
