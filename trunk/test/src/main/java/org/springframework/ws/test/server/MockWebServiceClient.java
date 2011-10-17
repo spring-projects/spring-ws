63ff6d6f57d321ad5fc9706e044db2fcdc8fea65
Arjen Poutsma <apoutsma@vmware.com>
2011-10-17 10:35:43 +0000

    SWS-714 - MockWebServiceClient.sendRequest swallows StackTrace of exceptions thrown during request processing

--- trunk/test/src/main/java/org/springframework/ws/test/server/MockWebServiceClient.java
+++ trunk/test/src/main/java/org/springframework/ws/test/server/MockWebServiceClient.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2005-2010 the original author or authors.
+ * Copyright 2005-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -181,6 +181,7 @@ public class MockWebServiceClient {
             return new MockWebServiceClientResponseActions(messageContext);
         }
         catch (Exception ex) {
+            logger.error("Could not send request", ex);
             fail(ex.getMessage());
             return null;
         }
@@ -209,6 +210,7 @@ public class MockWebServiceClient {
                 return this;
             }
             catch (IOException ex) {
+                logger.error("Could not match request", ex);
                 fail(ex.getMessage());
                 return null;
             }
