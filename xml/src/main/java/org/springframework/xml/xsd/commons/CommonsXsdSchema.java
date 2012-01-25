a4b7cea5836ba3551e8aa9e120922273c3f4f10a
Arjen Poutsma <apoutsma@vmware.com>
2012-01-25 09:35:59 +0000

    SWS-647 - PayloadValidatingInterceptor throws IllegalArgumentException when passed a schema collection

--- xml/src/main/java/org/springframework/xml/xsd/commons/CommonsXsdSchema.java
+++ xml/src/main/java/org/springframework/xml/xsd/commons/CommonsXsdSchema.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2005-2010 the original author or authors.
+ * Copyright 2005-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -95,8 +95,7 @@ public class CommonsXsdSchema implements XsdSchema {
     public Source getSource() {
         // try to use the the package-friendly XmlSchemaSerializer first, fall back to slower stream-based version
         try {
-            XmlSchemaSerializer serializer = (XmlSchemaSerializer) BeanUtils.instantiateClass(XmlSchemaSerializer.class)
-                    ;
+            XmlSchemaSerializer serializer = BeanUtils.instantiateClass(XmlSchemaSerializer.class);
             if (collection != null) {
                 serializer.setExtReg(collection.getExtReg());
             }
