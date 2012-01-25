341ba1cb59a1b6b65c02f92e963f7cad5cb17b3e
Arjen Poutsma <apoutsma@vmware.com>
2012-01-25 09:46:52 +0000

    Polishing

--- xml/src/main/java/org/springframework/xml/transform/TraxUtils.java
+++ xml/src/main/java/org/springframework/xml/transform/TraxUtils.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2005-2010 the original author or authors.
+ * Copyright 2005-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -256,7 +256,7 @@ public abstract class TraxUtils {
      * @param result   result to look at
      * @param callback the callback to invoke for each kind of result
      */
-    public static void doWithResult(Result result, ResultCallback callback) throws Exception{
+    public static void doWithResult(Result result, ResultCallback callback) throws Exception {
         if (result instanceof DOMResult) {
             callback.domResult(((DOMResult) result).getNode());
             return;
