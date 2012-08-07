786ebe2a8bda8be2a38256ba7bbd041d6c7319e0
Arjen Poutsma <apoutsma@vmware.com>
2012-08-07 10:20:14 +0000

    SWS-785 - MTOM sample should not use deprecated api

--- samples/mtom/server/src/main/java/org/springframework/ws/samples/mtom/ws/ImageRepositoryEndpoint.java
+++ samples/mtom/server/src/main/java/org/springframework/ws/samples/mtom/ws/ImageRepositoryEndpoint.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2007 the original author or authors.
+ * Copyright 2005-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,9 +16,7 @@
 
 package org.springframework.ws.samples.mtom.ws;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import javax.activation.DataHandler;
 import javax.xml.bind.JAXBElement;
 
 import org.springframework.util.Assert;
@@ -27,6 +25,8 @@ import org.springframework.ws.samples.mtom.schema.ObjectFactory;
 import org.springframework.ws.samples.mtom.service.ImageRepository;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 /** @author Arjen Poutsma */
 @Endpoint
@@ -43,13 +43,15 @@ public class ImageRepositoryEndpoint {
     }
 
     @PayloadRoot(localPart = "StoreImageRequest", namespace = "http://www.springframework.org/spring-ws/samples/mtom")
-    public void store(JAXBElement<Image> requestElement) throws IOException {
+    @ResponsePayload
+    public void store(@RequestPayload JAXBElement<Image> requestElement) throws IOException {
         Image request = requestElement.getValue();
         imageRepository.storeImage(request.getName(), request.getImage());
     }
 
     @PayloadRoot(localPart = "LoadImageRequest", namespace = "http://www.springframework.org/spring-ws/samples/mtom")
-    public JAXBElement<Image> load(JAXBElement<String> requestElement) throws IOException {
+    @ResponsePayload
+    public JAXBElement<Image> load(@RequestPayload JAXBElement<String> requestElement) throws IOException {
         String name = requestElement.getValue();
         Image response = new Image();
         response.setName(name);
