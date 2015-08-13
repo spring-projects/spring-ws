5ff07be1969a52f0db448625f0b7ac67d8717c59
Jamin Hitchcock <jamin.hitchcock@gmail.com>
2015-08-12 21:21:16 -0500

    SWS-886
    Update to WSS4J 2.0.x / XMLSec 2.0.x

---
+++ spring-ws-security/src/test/java/org/springframework/ws/soap/security/wss4j2/Wss4jInterceptorTestCase.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2005-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ws.soap.security.wss4j2;
+
+import org.springframework.ws.context.DefaultMessageContext;
+import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.soap.SoapMessage;
+import org.springframework.ws.soap.security.WsSecuritySecurementException;
+import org.springframework.ws.soap.security.WsSecurityValidationException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public abstract class Wss4jInterceptorTestCase extends Wss4jTestCase {
+
+	@Test
+	public void testHandleRequest() throws Exception {
+		SoapMessage request = loadSoap11Message("empty-soap.xml");
+		final Object requestMessage = getMessage(request);
+		SoapMessage validatedRequest = loadSoap11Message("empty-soap.xml");
+		final Object validatedRequestMessage = getMessage(validatedRequest);
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor() {
+			@Override
+			protected void secureMessage(SoapMessage soapMessage, MessageContext messageContext)
+					throws WsSecuritySecurementException {
+				fail("secure not expected");
+			}
+
+			@Override
+			protected void validateMessage(SoapMessage soapMessage, MessageContext messageContext)
+					throws WsSecurityValidationException {
+				assertEquals("Invalid message", requestMessage, getMessage(soapMessage));
+				setMessage(soapMessage, validatedRequestMessage);
+			}
+		};
+		MessageContext context = new DefaultMessageContext(request, getSoap11MessageFactory());
+		interceptor.handleRequest(context, null);
+		assertEquals("Invalid request", validatedRequestMessage, getMessage((SoapMessage) context.getRequest()));
+	}
+
+	@Test
+	public void testHandleResponse() throws Exception {
+		SoapMessage securedResponse = loadSoap11Message("empty-soap.xml");
+		final Object securedResponseMessage = getMessage(securedResponse);
+
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor() {
+
+			@Override
+			protected void secureMessage(SoapMessage soapMessage, MessageContext messageContext)
+					throws WsSecuritySecurementException {
+				setMessage(soapMessage, securedResponseMessage);
+			}
+
+			@Override
+			protected void validateMessage(SoapMessage soapMessage, MessageContext messageContext)
+					throws WsSecurityValidationException {
+				fail("validate not expected");
+			}
+
+		};
+		SoapMessage request = loadSoap11Message("empty-soap.xml");
+		MessageContext context = new DefaultMessageContext(request, getSoap11MessageFactory());
+		context.getResponse();
+		interceptor.handleResponse(context, null);
+		assertEquals("Invalid response", securedResponseMessage, getMessage((SoapMessage) context.getResponse()));
+	}
+
+}
---
+++ spring-ws-security/src/test/java/org/springframework/ws/soap/security/wss4j2/Wss4jMessageInterceptorTimestampTestCase.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2005-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ws.soap.security.wss4j2;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.springframework.ws.context.DefaultMessageContext;
+import org.springframework.ws.context.MessageContext;
+import org.springframework.ws.soap.SoapMessage;
+import org.springframework.ws.soap.security.WsSecurityValidationException;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import static org.junit.Assert.assertEquals;
+
+public abstract class Wss4jMessageInterceptorTimestampTestCase extends Wss4jTestCase {
+
+	@Test
+	public void testAddTimestamp() throws Exception {
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+		interceptor.setSecurementActions("Timestamp");
+		interceptor.afterPropertiesSet();
+		SoapMessage message = loadSoap11Message("empty-soap.xml");
+		MessageContext context = getSoap11MessageContext(message);
+		interceptor.secureMessage(message, context);
+		Document document = getDocument(message);
+		assertXpathExists("timestamp header not found",
+				"/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security/wsu:Timestamp", document);
+	}
+
+	@Test
+	public void testValidateTimestamp() throws Exception {
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+		interceptor.setValidationActions("Timestamp");
+		interceptor.afterPropertiesSet();
+		SoapMessage message = getMessageWithTimestamp();
+
+		MessageContext context = new DefaultMessageContext(message, getSoap11MessageFactory());
+		interceptor.validateMessage(message, context);
+		assertXpathNotExists("Security Header not removed", "/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security",
+				getDocument(message));
+	}
+
+	@Test(expected = WsSecurityValidationException.class)
+	public void testValidateTimestampWithExpiredTtl() throws Exception {
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+		interceptor.setValidationActions("Timestamp");
+		interceptor.afterPropertiesSet();
+		SoapMessage message = loadSoap11Message("expiredTimestamp-soap.xml");
+		MessageContext context = new DefaultMessageContext(message, getSoap11MessageFactory());
+		interceptor.validateMessage(message, context);
+	}
+
+
+	@Test
+	public void testSecureTimestampWithCustomTtl() throws Exception {
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+		interceptor.setSecurementActions("Timestamp");
+		interceptor.setTimestampStrict(true);
+		int ttlInSeconds = 1;
+		interceptor.setSecurementTimeToLive(ttlInSeconds);
+		interceptor.afterPropertiesSet();
+		SoapMessage message = loadSoap11Message("empty-soap.xml");
+		MessageContext context = new DefaultMessageContext(message, getSoap11MessageFactory());
+		interceptor.secureMessage(message, context);
+		
+		String created = xpathTemplate.evaluateAsString("/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security/wsu:Timestamp/wsu:Created/text()",
+				message.getEnvelope().getSource());
+		String expires = xpathTemplate.evaluateAsString("/SOAP-ENV:Envelope/SOAP-ENV:Header/wsse:Security/wsu:Timestamp/wsu:Expires/text()",
+				message.getEnvelope().getSource());
+
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+
+		long actualTtl = format.parse(expires).getTime() - format.parse(created).getTime();
+		assertEquals("invalid ttl", 1000 * ttlInSeconds, actualTtl);
+	}
+
+	private SoapMessage getMessageWithTimestamp() throws Exception {
+		Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+		interceptor.setSecurementActions("Timestamp");
+		interceptor.afterPropertiesSet();
+		SoapMessage message = loadSoap11Message("empty-soap.xml");
+		MessageContext context = getSoap11MessageContext(message);
+		interceptor.secureMessage(message, context);
+		return message;
+	}
+}
