e78a0741d9038e62398e81104eb2bd776b2f53c9
Greg Turnquist <gturnquist@pivotal.io>
2017-10-13 16:53:20 -0500

    SWS-998 - Fix artifactory deployment

--- deploy.bash
+++ deploy.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "2.x" ]; then
-	./mvnw -Pdistribute,snapshot,docs clean deploy
+	./mvnw -Pdistribute,snapshot,docs clean -Dmaven.test.skip=true deploy
 else
 	echo "We only deploy 'master' and '2.x' branches"
 fi
