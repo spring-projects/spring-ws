0a4a5589cb2b9c02af6d38ef50071c1d4bb42410
Greg Turnquist <gturnquist@pivotal.io>
2017-10-13 16:10:48 -0500

    SWS-998 - Switch off CircleCI workflow

--- deploy.bash
+++ deploy.bash
@@ -2,4 +2,6 @@
 
 if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "2.x" ]; then
 	./mvnw -Pdistribute,snapshot,docs clean deploy
+else
+	echo "We only deploy 'master' and '2.x' branches"
 fi
