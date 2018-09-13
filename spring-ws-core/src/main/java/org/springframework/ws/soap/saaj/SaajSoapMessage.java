6e2b03493a742de13b3d416367e5885c96182748
Jamin Hitchcock <jamin.hitchcock@gmail.com>
2018-09-13 12:00:34 -0500

    SWS-1018 - Move creation of an empty SOAP 1.1 envelope to the factory.
    To avoid SaajSoapMessage being created with an empty envelope, move the envelope creation process to SaajSoapMessageFactory.

--- spring-ws-core/src/main/java/org/springframework/ws/soap/saaj/SaajSoapMessage.java
+++ spring-ws-core/src/main/java/org/springframework/ws/soap/saaj/SaajSoapMessage.java
@@ -115,12 +115,6 @@ public class SaajSoapMessage extends AbstractSoapMessage {
 		saajMessage = soapMessage;
 		this.langAttributeOnSoap11FaultString = langAttributeOnSoap11FaultString;
 		this.messageFactory = messageFactory;
-		if (SoapVersion.SOAP_11.equals(getVersion())) {
-			MimeHeaders headers = soapMessage.getMimeHeaders();
-			if (ObjectUtils.isEmpty(headers.getHeader(TransportConstants.HEADER_SOAP_ACTION))) {
-				headers.addHeader(TransportConstants.HEADER_SOAP_ACTION, "\"\"");
-			}
-		}
 	}
 
 	/** Return the SAAJ {@code SOAPMessage} that this {@code SaajSoapMessage} is based on. */
