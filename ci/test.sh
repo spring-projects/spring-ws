65692073dae207e2b9d3e2f639b7deb2d3e43240
Greg Turnquist <gturnquist@pivotal.io>
2018-11-06 09:50:16 -0600

    SWS-1043 - Introduce Concourse

---
+++ ci/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
+
+rm -rf $HOME/.m2/repository/org/springframework/ws 2> /dev/null || :
+
+cd spring-ws-github
+
+./mvnw -P${PROFILE} clean test
