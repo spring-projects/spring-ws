7d50f0f9bccdcf9d293c287b4165976ea5434574
Greg L. Turnquist <gturnquist@vmware.com>
2023-01-04 16:03:09 -0600

    Introduce Spring WS BOM.
    The Spring WS BOM itself uses the flatten-maven-plugin to produce a POM file simplified for community consumption. The same plugin is ready to go for the other modules. But given the risk of breaking existing users, it will not be applied until Spring WS 4.1 is released (#1307).
    
    Resolves #1290.
    Related: #1205, #1285.

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -6,7 +6,7 @@ RELEASE=$1
 SNAPSHOT=$2
 
 # Bump up the version in pom.xml to the desired version and commit the change
-./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
+./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false -DprocessAllModules=true -DoldVersion='*'
 git add .
 git commit --message "Releasing Spring Web Services v$RELEASE"
 
@@ -14,7 +14,7 @@ git commit --message "Releasing Spring Web Services v$RELEASE"
 git tag -s v$RELEASE -m "v$RELEASE"
 
 # Bump up the version in pom.xml to the next snapshot
-./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
+./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false -DprocessAllModules=true -DoldVersion='*'
 git add .
 git commit --message "Continue development on v$SNAPSHOT"
 
