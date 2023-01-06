9f99e26a4f77a44283843fab58797647a148d31a
Mathias Geat <mathias@ailoo.net>
2023-01-06 12:57:30 +0100

    Update license headers + docblocks

--- spring-ws-core/src/main/java/org/springframework/ws/transport/http/OkHttpConnection.java
+++ spring-ws-core/src/main/java/org/springframework/ws/transport/http/OkHttpConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2021 the original author or authors.
+ * Copyright 2005-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ import org.springframework.ws.transport.WebServiceConnection;
  * Implementation of {@link WebServiceConnection} that is based on OkHttp.
  *
  * @author Mathias Geat
- * @since 3.1.0
+ * @since 4.1.0
  */
 public class OkHttpConnection extends AbstractHttpSenderConnection {
 
--- spring-ws-core/src/main/java/org/springframework/ws/transport/http/OkHttpMessageSender.java
+++ spring-ws-core/src/main/java/org/springframework/ws/transport/http/OkHttpMessageSender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2021 the original author or authors.
+ * Copyright 2005-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import org.springframework.ws.transport.WebServiceConnection;
  *
  * @author Mathias Geat
  * @see OkHttpClient
- * @since 3.1.0
+ * @since 4.1.0
  */
 public class OkHttpMessageSender extends AbstractHttpWebServiceMessageSender {
 
