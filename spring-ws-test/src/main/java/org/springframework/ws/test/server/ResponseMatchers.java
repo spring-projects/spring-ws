c5fb12c20e8e001dd7a755ad2efda9365a2c261c
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2023-08-15 20:07:24 +0200

    Fix forgotten import for XMLUnit2 upgrade
    When the XMLUnit2 upgrade was done (commit: c39d3d1) this import was
    forgotten.
    
    It causes the bug reported with XMLUnit1 (issue: #1193) to still be
    present when using `ResultMatchers#soapEnvelope(Source)`.
    For more infos: https://github.com/spring-projects/spring-ws/issues/1193#issuecomment-1650162836
    
    Here comes the fix for it.

--- spring-ws-test/src/main/java/org/springframework/ws/test/server/ResponseMatchers.java
+++ spring-ws-test/src/main/java/org/springframework/ws/test/server/ResponseMatchers.java
@@ -31,9 +31,9 @@ import org.springframework.ws.FaultAwareWebServiceMessage;
 import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.soap.SoapVersion;
 import org.springframework.ws.test.support.matcher.SchemaValidatingMatcher;
-import org.springframework.ws.test.support.matcher.SoapEnvelopeDiffMatcher;
 import org.springframework.ws.test.support.matcher.SoapHeaderMatcher;
 import org.springframework.ws.test.support.matcher.xmlunit2.PayloadDiffMatcher;
+import org.springframework.ws.test.support.matcher.xmlunit2.SoapEnvelopeDiffMatcher;
 import org.springframework.xml.transform.ResourceSource;
 
 /**
