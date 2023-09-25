3ec7f58f84beb4a4d0bbc3d10389efba5d300bdf
Greg L. Turnquist <gturnquist@vmware.com>
2023-09-25 10:08:13 -0500

    Polishing.

--- ci/rc-release.sh
+++ ci/rc-release.sh
@@ -6,7 +6,7 @@ STAGING_REPOSITORY_ID=$1
 
 echo 'Release to Maven central...'
 
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw \
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home --add-opens=java.base/java.util=ALL-UNNAMED " ./mvnw \
     -s settings.xml \
     -Pcentral  \
     -DstagingRepositoryId="${STAGING_REPOSITORY_ID}" \
