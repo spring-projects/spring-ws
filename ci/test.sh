27b07a57ffac93fa72f7687b0c56f43b4153e8ba
Eric Haag <ehaag@gradle.com>
2024-08-07 08:44:47 -0500

    Migrate build to Develocity Conventions Maven extension. (#1426)

--- ci/test.sh
+++ ci/test.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-export GRADLE_ENTERPRISE_CACHE_USERNAME=${GRADLE_ENTERPRISE_CACHE_USR}
-export GRADLE_ENTERPRISE_CACHE_PASSWORD=${GRADLE_ENTERPRISE_CACHE_PSW}
-
 MAVEN_OPTS="-Duser.name=spring-builds+jenkins -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml \
   -P${PROFILE} clean dependency:list test -Dsort -B -U
