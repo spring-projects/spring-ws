67fc381665d4699c80cac5bcab70cc96eccf9a84
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-12-17 17:03:49 +0200

    Fix problem with BeanPostProcessing
    Change endpoint mapping from BeanPostProcessor to SmartInitializingSingleton
    Added Test and updated test.sh to invoke with separate profile.
    Fixes #1435

--- ci/test.sh
+++ ci/test.sh
@@ -8,3 +8,7 @@ export GRADLE_ENTERPRISE_CACHE_PASSWORD=${GRADLE_ENTERPRISE_CACHE_PSW}
 MAVEN_OPTS="-Duser.name=spring-builds+jenkins -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml \
   -P${PROFILE} clean dependency:list test -Dsort -B -U
+# Provides for testing org.springframework.ws.observation.ObservationInWsConfigurerTests separately
+MAVEN_OPTS="-Duser.name=spring-builds+jenkins -Duser.home=/tmp/jenkins-home" \
+  ./mvnw -s settings.xml \
+  -P-default,observation clean dependency:list test -Dsort -B -U
