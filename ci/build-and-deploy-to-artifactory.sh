229d77f825bc901b0c3b1cfd148b74dcc3e08df8
Corneil du Plessis <corneil.du-plessis@broadcom.com>
2024-12-18 15:22:15 +0200

    Update pipeline and docker image

--- ci/build-and-deploy-to-artifactory.sh
+++ ci/build-and-deploy-to-artifactory.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 
 RELEASE_TYPE=$1
 
-export GRADLE_ENTERPRISE_CACHE_USERNAME=${GRADLE_ENTERPRISE_CACHE_USR}
-export GRADLE_ENTERPRISE_CACHE_PASSWORD=${GRADLE_ENTERPRISE_CACHE_PSW}
-
 echo 'Deploying to Artifactory...'
 
 MAVEN_OPTS="-Duser.name=spring-builds+jenkins -Duser.home=/tmp/jenkins-home" ./mvnw \
