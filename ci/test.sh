b40c664fb38839a588963ab2ca61a8178845c851
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-02-28 16:30:04 +0100

    Remove Jenkins pipeline
    This commit removes the Jenkins pipeline in preparation to the move to
    GitHub Actions.
    
    See gh-1461

--- ci/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-MAVEN_OPTS="-Duser.name=spring-builds+jenkins -Duser.home=/tmp/jenkins-home" \
-  ./mvnw -s settings.xml \
-  -P${PROFILE} clean dependency:list test -Dsort -B -U
